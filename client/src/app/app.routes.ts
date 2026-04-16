f68dd46f47ac0bd18838e53c1d241ea634104ff3
Shukri <muhd.shukri@wira.org>
2026-04-16 15:03:31 +0800

    add calendar component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { About } from './page/about/about';
 import { Faq } from './page/faq/faq';
 import { Pricing } from './page/pricing/pricing';
 import { SsoLoading } from './page/sso-loading/sso-loading';
+import { Calendar } from './page/calendar/calendar';
 
 export const routes: Routes = [
   { path: '', component: SsoLoading },
   { path: 'main', component: Main },
   { path: 'chatbot', component: Chatbot, outlet: 'sidebar' },
+  { path: 'calendar', component: Calendar, outlet: 'sidebar' },
   { path: 'about', component: About, outlet: 'sidebar' },
   { path: 'faq', component: Faq, outlet: 'sidebar' },
   { path: 'pricing', component: Pricing, outlet: 'sidebar' },
